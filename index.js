effdef224342c51acf9fa69a6c1e6c236f348cd2
Zimkhitha <zimkhitha@projectcodex.co>
2017-04-25 18:21:32 +0200

    nodemailer working

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 var express = require('express'),
-  exphbs = require('express-handlebars'),
-  session = require('express-session'),
-  bodyParser = require('body-parser'),
-  mysql = require('mysql'),
-  myConnection = require('express-myconnection'),
-  questionnare = require('./routes/questionnare'),
-  answers = require('./routes/answers'),
-  signup = require('./routes/signup'),
-  flash = require('express-flash'),
-  record = require('./routes/record'),
-  middleware = require('./middlewares/server'),
-  login = require('./routes/login'),
-  nodemailer = require('nodemailer'),
-  hbs = require('nodemailer-express-handlebars'),
-  cookieParser = require('cookie-parser');
+    exphbs = require('express-handlebars'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    mysql = require('mysql'),
+    myConnection = require('express-myconnection'),
+    questionnare = require('./routes/questionnare'),
+    answers = require('./routes/answers'),
+    signup = require('./routes/signup'),
+    flash = require('express-flash'),
+    record = require('./routes/record'),
+    middleware = require('./middlewares/server'),
+    login = require('./routes/login'),
+    nodemailer = require('nodemailer'),
+    hbs = require('nodemailer-express-handlebars'),
+    cookieParser = require('cookie-parser');
 
 var app = express();
 var dbOptions = {
-host: 'localhost',
-user: 'root',
-password: 'p@$$w0rd',
-database: 'nmldb'
+    host: 'localhost',
+    user: 'root',
+    password: 'p@$$w0rd',
+    database: 'nmldb'
 }
 
 app.engine('handlebars', exphbs({
-  defaultLayout: 'base'
+    defaultLayout: 'base'
 }));
 
 app.set('view engine', 'handlebars');
@@ -35,92 +35,126 @@ app.use(flash());
 app.use(myConnection(mysql, dbOptions, 'single'));
 
 app.use(cookieParser());
+
 app.use(session({
-  secret: 'keyboard cat',
-  cookie: {
-    maxAge: 60000 * 30 // expire after 30 minutes
-  }
+    secret: 'keyboard cat',
+    cookie: {
+        maxAge: 60000 * 30 // expire after 30 minutes
+    }
 }));
-// app.use(function(req, res, next) {
-//   res.locals.currentUser = req.session.employee_id;
-//    console.log(res.locals.currentUser);
-//   next();
-// });
+//make employee id available in templates
+app.use(function(req, res, next) {
+
+    if (!req.session.employee
+        && req.path !== '/login'
+        && req.path !== '/signup'){
+
+      return res.redirect('/login');
+    }
+
+    next();
+});
+
+app.use(function(req, res, next) {
+
+    if (req.session.employee){
+      res.locals.currentUser = req.session.employee;
+    }
+
+    //console.log(req.locals);
+    next();
+});
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: true
-  }))
-  // parse application/json
+}))
+// parse application/json
 app.use(bodyParser.json());
 
 var mailer = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: 'zimkhitha@projectcodex.co',
-    password: 'Zimmat321'
-  }
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'zimkhitha@projectcodex.co',
+        pass: 'ymgaxajislsjngqc'
+    }
 });
 
-mailer.use('compile',hbs({
-  viewPath: 'views',
-  extName: '.handlebars'
+mailer.use('compile', hbs({
+    viewPath: 'views',
+    extName: '.handlebars'
 }))
 
 app.get('/', function(req, res, next) {
-    res.render('/questionnare');
+    res.render('questionnare');
 })
 
 app.get('/sendEmail', function(req, res, next) {
-  mailer.sendMail({
-    from:'zimkhitha@projectcodex.co',
-    to:'zimmatshangaza@gmail.com',
-    subject:'Questionnare',
-    template:'email',
-    function(err,res){
-    if(err){
-      res.send("bad email");
-    }
-    res.send("good email");
-  }
-});
+
+    req.getConnection(function(err, connection) {
+        connection.query('SELECT * FROM employees', [], function(err, results) {
+            //console.log(results);
+            if (err) return next(err);
+
+            if (results.length == 0) {
+                req.flash("warning", "no recipient");
+            } else {
+
+                var maillist = results.map(function(user) {
+                    return user.email;
+                });
+
+                mailer.sendMail({
+                    from: 'zimkhitha@projectcodex.co',
+                    to: maillist,
+                    subject: 'Questionnare',
+                    template: 'email',
+                }, function(err, emailResponse) {
+                    console.log(emailResponse);
+                    if (err) {
+                        res.send("bad email");
+                    }
+                    req.flash("message", "email sent");
+                    return res.redirect("/");
+
+                });
+
+            };
+        });
+    });
 });
 
 app.get("/signup", middleware.loggedOut, function(req, res, next) {
-  res.render("signup");
+    res.render("signup");
 });
+
 app.get("/login", middleware.loggedOut, function(req, res) {
-  res.render("login", {
-    showNavBar: false
-  });
+    res.render("login", {
+        showNavBar: false
+    });
 });
-var checkUser = function(req, res, next) {
-  if (req.session.employee) {
-    return next();
-  }
-  res.redirect("/login");
-};
+
 app.post("/login", login.myLogin);
 
-app.get("/", checkUser, function(req, res) {
-  res.render("home", {
-    employee: req.session.employee,
-    is_admin: req.session.user.is_admin
-  })
+app.get("/", function(req, res) {
+    res.render("home", {
+        employee: req.session.employee,
+        is_admin: req.session.user.is_admin
+    })
 });
 
 app.get("/logout", function(req, res) {
-  delete req.sess;
-  res.redirect("/login");
+    delete req.session.employee;
+    res.redirect("/login");
 })
 
 
 function errorHandler(err, req, res, next) {
-  res.status(500);
-  res.render('error', {
-    error: err
-  });
+    res.status(500);
+    res.render('error', {
+        error: err
+    });
 }
 
 
@@ -128,18 +162,18 @@ function errorHandler(err, req, res, next) {
 
 app.get('/signup/addUser', signup.showSignup);
 app.post('/signup/addUser', signup.add);
-app.get('/questionnare',questionnare.display);
-app.post('/questionnare/createQuestions',questionnare.createQuestions);
+app.get('/questionnare', questionnare.display);
+app.post('/questionnare/createQuestions', questionnare.createQuestions);
 app.get('/answers', answers.show);
 app.get('/record', record.showAllRecords);
-app.get('/record',record.showRecord);
-app.get('/record/edit/answers_id',record.getRecord);
-app.post('/record/update/answers_id',record.updateRecord);
+app.get('/record', record.showRecord);
+app.get('/record/edit/answers_id', record.getRecord);
+app.post('/record/update/answers_id', record.updateRecord);
 app.post('/answers/add', answers.add);
 
 
 var portNumber = process.env.PORT_NR || 5000;
 
 app.listen(portNumber, function() {
-  console.log('App running on:', portNumber);
+    console.log('App running on:', portNumber);
 });
--- routes/login.js
+++ routes/login.js
@@ -1,37 +1,43 @@
 var mysql = require('mysql');
 
 var rolesMap = {
-  "Zee": "Viewer",
-  "Zimkhitha": "admin"
+    "Zee": "Viewer",
+    "Zimkhitha": "admin"
 };
 var sess;
-exports.myLogin = function(req,res,next){
-  sess = req.session;
-var employee = {
-  name: req.body.name,
-  password: req.body.password,
-}
+exports.myLogin = function(req, res, next) {
+    // sess = req.session;
+    var employee = {
+        name: req.body.name,
+        password: req.body.password,
+    }
 
-req.getConnection(function(err, connection) {
-  connection.query('SELECT * FROM employees WHERE name = ?', [employee.name], function(err, results) {
-    if (err) return next(err);
-    if (results.length == 0) {
-      req.flash("warning", "Signup to proceed");
-      res.redirect("/login")
-    } else {
-      var dbPassword = results[0];
-        if (employee.password === dbPassword.password) {
-          sess = {
-            name: req.body.name
-          }
-           console.log(sess.name + ' ' + 'is in session');
-          res.redirect("/questionnare");
-        } else {
-          req.flash("warning", "incorrect password");
-          return res.redirect("/login");
-        }
-      }
+    req.getConnection(function(err, connection) {
+        connection.query('SELECT * FROM employees WHERE name = ?', [employee.name], function(err, results) {
+            if (err) return next(err);
+            if (results.length == 0) {
+                req.flash("warning", "Signup to proceed");
+                res.redirect("/login")
+            } else {
+                var currentEmployee = results[0];
+
+                console.log(currentEmployee);
+
+                if (employee.password === currentEmployee.password) {
+                    // sess = {
+                    //   name: req.body.name
+                    // }
+                    //  console.log(sess.name + ' ' + 'is in session');
+
+                    req.session.employee = currentEmployee;
+
+                    res.redirect("/questionnare");
+                } else {
+                    req.flash("warning", "incorrect password");
+                    return res.redirect("/login");
+                }
+            }
+        });
     });
-  });
 
 };
